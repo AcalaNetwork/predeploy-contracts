3394557c458d28cd722e57bf8d3222546db1085d
Shaopeng Wang <spxwang@gmail.com>
2020-10-23 12:26:17 +1300

    MultiCurrency: update return types.

--- contracts/MultiCurrency.sol
+++ contracts/MultiCurrency.sol
@@ -2,54 +2,66 @@
 pragma solidity ^0.5.0;
 
 library MultiCurrency {
-  function totalSupply(uint256 currencyId) internal returns(uint256[1] memory p) {
-      uint256[1] memory input;
+    function totalSupply(uint256 currencyId) internal view returns (uint256) {
+        uint256[1] memory input;
 
-      uint256 x = 0 << 32;
-      x += currencyId;
+        uint256 x = 0 << 32;
+        x += currencyId;
 
-      input[0] = x << 216;
+        input[0] = x << 216;
 
-      assembly {
-        if iszero(staticcall(gas, 0x0000000000000000001, input, 0x20, p, 0x20)) {
-            revert(0,0)
+        uint256[1] memory output;
+
+        assembly {
+            if iszero(
+                staticcall(gas, 0x0000000000000000001, input, 0x20, output, 0x20)
+            ) {
+                revert(0, 0)
+            }
         }
-      }
-      return p;
-  }
 
-  function balanceOf(uint256 currencyId, address account) internal returns(uint256[1] memory p) {
-      uint256[2] memory input;
+        return output[0];
+    }
 
-      uint256 x = 1 << 32;
-      x += currencyId;
+    function balanceOf(uint256 currencyId, address account) internal view returns (uint256) {
+        uint256[2] memory input;
 
-      input[0] = x << 216;
-      input[1] = uint256(account) << 96;
+        uint256 x = 1 << 32;
+        x += currencyId;
 
-      assembly {
-        if iszero(staticcall(gas, 0x0000000000000000001, input, 0x40, p, 0x20)) {
-            revert(0,0)
+        input[0] = x << 216;
+        input[1] = uint256(account) << 96;
+
+        uint256[1] memory output;
+
+        assembly {
+            if iszero(
+                staticcall(gas, 0x0000000000000000001, input, 0x40, output, 0x20)
+            ) {
+                revert(0, 0)
+            }
         }
-      }
-      return p;
-  }
 
-  function transfer(uint256 currencyId, address sender, address recipient, uint256 amount) internal {
-      uint256[4] memory input;
+        return output[0];
+    }
 
-      uint256 x = 2 << 32;
-      x += currencyId;
+    function transfer(uint256 currencyId, address sender, address recipient, uint256 amount) internal {
+        uint256[4] memory input;
 
-      input[0] = x << 216;
-      input[1] = uint256(sender) << 96;
-      input[2] = uint256(recipient) << 96;
-      input[3] = amount << 128;
+        uint256 x = 2 << 32;
+        x += currencyId;
 
-      assembly {
-        if iszero(staticcall(gas, 0x0000000000000000001, input, 0x80, 0x00, 0x00)) {
-            revert(0,0)
+        input[0] = x << 216;
+        input[1] = uint256(sender) << 96;
+        input[2] = uint256(recipient) << 96;
+        input[3] = amount << 128;
+
+        assembly {
+            if iszero(
+                staticcall(gas, 0x0000000000000000001, input, 0x80, 0x00, 0x00)
+            ) {
+                revert(0, 0)
+            }
         }
-      }
-  }
+    }
 }
