971de9875c929c7eed0be286c4dd6e739864d23c
Shaopeng Wang <spxwang@gmail.com>
2020-11-12 14:41:20 +1300

    MultiCurrency precompile address.

--- contracts/MultiCurrency.sol
+++ contracts/MultiCurrency.sol
@@ -16,7 +16,7 @@ library MultiCurrency {
 
         assembly {
             if iszero(
-                staticcall(gas, 0x0000000000000000001, input, 0x20, output, 0x20)
+                staticcall(gas, 0x0000000000000000400, input, 0x20, output, 0x20)
             ) {
                 revert(0, 0)
             }
@@ -38,7 +38,7 @@ library MultiCurrency {
 
         assembly {
             if iszero(
-                staticcall(gas, 0x0000000000000000001, input, 0x40, output, 0x20)
+                staticcall(gas, 0x0000000000000000400, input, 0x40, output, 0x20)
             ) {
                 revert(0, 0)
             }
@@ -47,7 +47,7 @@ library MultiCurrency {
         return output[0];
     }
 
-    function transfer(uint256 currencyId, address sender, address recipient, uint256 amount) internal {
+    function transfer(uint256 currencyId, address sender, address recipient, uint256 amount) internal view {
         uint256[4] memory input;
 
         uint256 x = 2 << 32;
@@ -60,7 +60,7 @@ library MultiCurrency {
 
         assembly {
             if iszero(
-                staticcall(gas, 0x0000000000000000001, input, 0x80, 0x00, 0x00)
+                staticcall(gas, 0x0000000000000000400, input, 0x80, 0x00, 0x00)
             ) {
                 revert(0, 0)
             }
