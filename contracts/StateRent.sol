dd70db90d5213843d609949c2e40f9f4bff76b5b
zjb0807 <zjb0807@qq.com>
2021-01-13 11:12:53 +0800

    update transfer maintainer

--- contracts/StateRent.sol
+++ contracts/StateRent.sol
@@ -3,27 +3,14 @@ pragma solidity ^0.5.0;
 import "./StateRentLib.sol";
 
 contract StateRent {
-    event AddStorageQuota(address indexed contract_address, uint256 value);
-    event RemoveStorageQuota(address indexed contract_address, uint256 value);
-    event RequestedTransferMaintainer(address indexed sender, address indexed contract_address);
-    event CanceledTransferMaintainer(address indexed sender, address indexed contract_address);
-    event ConfirmedTransferMaintainer(address indexed contract_address, address indexed new_maintainer);
-    event RejectedTransferMaintainer(address indexed contract_address, address indexed invalid_maintainer);
+    event TransferedMaintainer(address indexed contract_address, address indexed new_maintainer);
     
-    function contractExistentialDeposit() public view returns (uint256) {
-        return StateRentLib.contractExistentialDeposit();
-    }
-
-    function transferMaintainerDeposit() public view returns (uint256) {
-        return StateRentLib.transferMaintainerDeposit();
+    function newContractExtraBytes() public view returns (uint256) {
+        return StateRentLib.newContractExtraBytes();
     }
 
     function storageDepositPerByte() public view returns (uint256) {
         return StateRentLib.storageDepositPerByte();
-    }
-
-    function storageDefaultQuota() public view returns (uint256) {
-        return StateRentLib.storageDefaultQuota();
     }
 
     function maintainerOf(address contract_address)
@@ -42,100 +29,15 @@ contract StateRent {
         return StateRentLib.deploymentFee();
     }
 
-    function addStorageQuota(
-        address contract_address,
-        uint256 bytes_size
-    ) public returns (bool) {
-        _addStorageQuota(msg.sender, contract_address, bytes_size);
-        return true;
-    }
-
-    function removeStorageQuota(
-        address contract_address,
-        uint256 bytes_size
-    ) public returns (bool) {
-        _removeStorageQuota(msg.sender, contract_address, bytes_size);
-        return true;
-    }
-
-    function requestTransferMaintainer(
-        address contract_address
-    ) public returns (bool) {
-        _requestTransferMaintainer(msg.sender, contract_address);
-        return true;
-    }
-
-    function cancelTransferMaintainer(
-        address contract_address
-    ) public returns (bool) {
-        _cancelTransferMaintainer(msg.sender, contract_address);
-        return true;
-    }
-
-    function confirmTransferMaintainer(
+    function transferMaintainer(
         address contract_address,
         address new_maintainer
     ) public returns (bool) {
-        _confirmTransferMaintainer(msg.sender, contract_address, new_maintainer);
+        _transferMaintainer(msg.sender, contract_address, new_maintainer);
         return true;
     }
 
-    function rejectTransferMaintainer(
-        address contract_address,
-        address invalid_maintainer
-    ) public returns (bool) {
-        _rejectTransferMaintainer(msg.sender, contract_address, invalid_maintainer);
-        return true;
-    }
-    
-    function _addStorageQuota(
-        address sender,
-        address contract_address,
-        uint256 bytes_size
-    ) internal {
-        require(sender != address(0), "StateRent: the sender is the zero address");
-        require(contract_address != address(0), "StateRent: the contract_address is the zero address");
-
-        StateRentLib.addStorageQuota(sender, contract_address, bytes_size);
-        emit AddStorageQuota(contract_address, bytes_size);
-
-    }
-    
-    function _removeStorageQuota(
-        address sender,
-        address contract_address,
-        uint256 bytes_size
-    ) internal {
-        require(sender != address(0), "StateRent: the sender is the zero address");
-        require(contract_address != address(0), "StateRent: the contract_address is the zero address");
-
-        StateRentLib.removeStorageQuota(msg.sender, contract_address, bytes_size);
-        emit RemoveStorageQuota(contract_address, bytes_size);
-    }
-
-    function _requestTransferMaintainer(
-        address sender,
-        address contract_address
-    ) internal {
-        require(sender != address(0), "StateRent: the sender is the zero address");
-        require(contract_address != address(0), "StateRent: the contract_address is the zero address");
-        
-        StateRentLib.requestTransferMaintainer(msg.sender, contract_address);
-        emit RequestedTransferMaintainer(sender, contract_address);
-    }
-
-    function _cancelTransferMaintainer(
-        address sender,
-        address contract_address
-    ) internal {
-        require(sender != address(0), "StateRent: the sender is the zero address");
-        require(contract_address != address(0), "StateRent: the contract_address is the zero address");
-
-        StateRentLib.cancelTransferMaintainer(msg.sender, contract_address);
-        emit CanceledTransferMaintainer(sender, contract_address);
-    }
-
-    function _confirmTransferMaintainer(
+    function _transferMaintainer(
         address sender,
         address contract_address,
         address new_maintainer
@@ -144,21 +46,8 @@ contract StateRent {
         require(contract_address != address(0), "StateRent: the contract_address is the zero address");
         require(new_maintainer != address(0), "StateRent: the new_maintainer is the zero address");
 
-        StateRentLib.confirmTransferMaintainer(msg.sender, contract_address, new_maintainer);
-        emit ConfirmedTransferMaintainer(contract_address, new_maintainer);
-    }
-    
-    function _rejectTransferMaintainer(
-        address sender,
-        address contract_address,
-        address invalid_maintainer
-    ) internal {
-        require(sender != address(0), "StateRent: the sender is the zero address");
-        require(contract_address != address(0), "StateRent: the contract_address is the zero address");
-        require(invalid_maintainer != address(0), "StateRent: the invalid_maintainer is the zero address");
-
-        StateRentLib.rejectTransferMaintainer(msg.sender, contract_address, invalid_maintainer);
-        emit RejectedTransferMaintainer(contract_address, invalid_maintainer);
+        StateRentLib.transferMaintainer(msg.sender, contract_address, new_maintainer);
+        emit TransferedMaintainer(contract_address, new_maintainer);
     }
 }
 
--- contracts/StateRentLib.sol
+++ contracts/StateRentLib.sol
@@ -1,7 +1,7 @@
 pragma solidity ^0.5.0;
 
 library StateRentLib {
-    function contractExistentialDeposit() internal view returns (uint256) {
+    function newContractExtraBytes() internal view returns (uint256) {
         uint256[1] memory input;
 
         input[0] = 0;
@@ -18,44 +18,10 @@ library StateRentLib {
         return output[0];
     }
 
-    function transferMaintainerDeposit() internal view returns (uint256) {
-        uint256[1] memory input;
-
-        input[0] = 1;
-
-        uint256[1] memory output;
-
-        assembly {
-            if iszero(
-                staticcall(gas, 0x0000000000000000402, input, 0x20, output, 0x20)
-            ) {
-                revert(0, 0)
-            }
-        }
-        return output[0];
-    }
-
     function storageDepositPerByte() internal view returns (uint256) {
         uint256[1] memory input;
 
-        input[0] = 2;
-
-        uint256[1] memory output;
-
-        assembly {
-            if iszero(
-                staticcall(gas, 0x0000000000000000402, input, 0x20, output, 0x20)
-            ) {
-                revert(0, 0)
-            }
-        }
-        return output[0];
-    }
-
-    function storageDefaultQuota() internal view returns (uint256) {
-        uint256[1] memory input;
-
-        input[0] = 3;
+        input[0] = 1;
 
         uint256[1] memory output;
 
@@ -76,7 +42,7 @@ library StateRentLib {
     {
         uint256[2] memory input;
 
-        input[0] = 4;
+        input[0] = 2;
         input[1] = uint256(contract_address);
 
         uint256[1] memory output;
@@ -94,7 +60,7 @@ library StateRentLib {
     function developerDeposit() internal view returns (uint256) {
         uint256[1] memory input;
 
-        input[0] = 5;
+        input[0] = 3;
 
         uint256[1] memory output;
 
@@ -111,7 +77,7 @@ library StateRentLib {
     function deploymentFee() internal view returns (uint256) {
         uint256[1] memory input;
 
-        input[0] = 6;
+        input[0] = 4;
 
         uint256[1] memory output;
 
@@ -125,118 +91,17 @@ library StateRentLib {
         return output[0];
     }
 
-    function addStorageQuota(
-        address sender,
-        address contract_address,
-        uint256 bytes_size
-    ) internal view {
-        uint256[4] memory input;
-
-        input[0] = 128;
-        input[1] = uint256(sender);
-        input[2] = uint256(contract_address);
-        input[3] = bytes_size;
-
-        assembly {
-            if iszero(
-                staticcall(gas, 0x0000000000000000402, input, 0x80, 0x00, 0x00)
-            ) {
-                revert(0, 0)
-            }
-        }
-    }
-
-    function removeStorageQuota(
-        address sender,
-        address contract_address,
-        uint256 bytes_size
-    ) internal view {
-        uint256[4] memory input;
-
-        input[0] = 129;
-        input[1] = uint256(sender);
-        input[2] = uint256(contract_address);
-        input[3] = bytes_size;
-
-        assembly {
-            if iszero(
-                staticcall(gas, 0x0000000000000000402, input, 0x80, 0x00, 0x00)
-            ) {
-                revert(0, 0)
-            }
-        }
-    }
-
-    function requestTransferMaintainer(
-        address sender,
-        address contract_address
-    ) internal view {
-        uint256[3] memory input;
-
-        input[0] = 130;
-        input[1] = uint256(sender);
-        input[2] = uint256(contract_address);
-
-        assembly {
-            if iszero(
-                staticcall(gas, 0x0000000000000000402, input, 0x60, 0x00, 0x00)
-            ) {
-                revert(0, 0)
-            }
-        }
-    }
-
-    function cancelTransferMaintainer(
-        address sender,
-        address contract_address
-    ) internal view {
-        uint256[3] memory input;
-
-        input[0] = 131;
-        input[1] = uint256(sender);
-        input[2] = uint256(contract_address);
-
-        assembly {
-            if iszero(
-                staticcall(gas, 0x0000000000000000402, input, 0x60, 0x00, 0x00)
-            ) {
-                revert(0, 0)
-            }
-        }
-    }
-
-    function confirmTransferMaintainer(
+    function transferMaintainer(
         address sender,
         address contract_address,
         address new_maintainer
     ) internal view {
         uint256[4] memory input;
 
-        input[0] = 132;
+        input[0] = 128;
         input[1] = uint256(sender);
         input[2] = uint256(contract_address);
         input[3] = uint256(new_maintainer);
-
-        assembly {
-            if iszero(
-                staticcall(gas, 0x0000000000000000402, input, 0x80, 0x00, 0x00)
-            ) {
-                revert(0, 0)
-            }
-        }
-    }
-
-    function rejectTransferMaintainer(
-        address sender,
-        address contract_address,
-        address invalid_maintainer
-    ) internal view {
-        uint256[4] memory input;
-
-        input[0] = 133;
-        input[1] = uint256(sender);
-        input[2] = uint256(contract_address);
-        input[3] = uint256(invalid_maintainer);
 
         assembly {
             if iszero(
