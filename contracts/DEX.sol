1d14a9fcfd9a5fa2cf33624c9449a068583ef882
Ermal Kaleci <ermalkaleci@gmail.com>
2021-02-08 15:03:39 +0100

    add docs

--- contracts/DEX.sol
+++ contracts/DEX.sol
@@ -4,6 +4,10 @@ import "./SystemContract.sol";
 import "./MultiCurrency.sol";
 
 contract DEX is SystemContract {
+    /**
+     * @dev Get liquidity of the currency_id_a and currency_id_b.
+     * Returns (liquidity_a, liquidity_b)
+     */
     function getLiquidity(address tokenA, address tokenB)
     public
     view
@@ -35,6 +39,10 @@ contract DEX is SystemContract {
         return (output[0], output[1]);
     }
 
+    /**
+     * @dev Swap with exact supply.
+     * Returns (target_amount)
+     */
     function swapWithExactSupply(address tokenA, address tokenB, uint256 supplyAmount, uint256 minTargetAmount)
     public
     view
