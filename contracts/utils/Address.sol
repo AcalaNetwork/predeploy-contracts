9809fb6e91c239a525901fbd9ff2a879dce0612b
zjb0807 <zjb0807@qq.com>
2021-03-01 03:02:35 +0800

    add library function

--- contracts/utils/Address.sol
+++ contracts/utils/Address.sol
@@ -7,4 +7,23 @@ library ADDRESS {
 	address public constant Oracle = 0x0000000000000000000000000000000000000801;
 	address public constant ScheduleCall = 0x0000000000000000000000000000000000000802;
 	address public constant DEX = 0x0000000000000000000000000000000000000803;
+
+	function getACAAddress() public pure returns(address) {
+		return this.[0];
+	}
+	function getAUSDAddress() public pure returns(address) {
+		return this.[0];
+	}
+	function getStateRentAddress() public pure returns(address) {
+		return this.[0];
+	}
+	function getOracleAddress() public pure returns(address) {
+		return this.[0];
+	}
+	function getScheduleCallAddress() public pure returns(address) {
+		return this.[0];
+	}
+	function getDEXAddress() public pure returns(address) {
+		return this.[0];
+	}
 }
