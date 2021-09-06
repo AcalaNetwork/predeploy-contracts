6b43ef11d6c74e685e27db04583cc75230d13bd3
zjb0807 <zjb0807@qq.com>
2021-09-06 10:55:57 +0800

    add license and update solidity 0.8.0

--- contracts/utils/Migrations.sol
+++ contracts/utils/Migrations.sol
@@ -1,5 +1,6 @@
-// SPDX-License-Identifier: MIT
-pragma solidity >=0.4.22 <0.8.0;
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+pragma solidity ^0.8.0;
 
 contract Migrations {
   address public owner = msg.sender;
