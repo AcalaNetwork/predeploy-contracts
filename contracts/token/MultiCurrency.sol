da11867983449712aea63f4f85ce8fed81cd6e9a
zjb0807 <zjb0807@qq.com>
2021-09-08 12:07:27 +0800

    merge 7a87f2b0e470e1b913a8f8417cf903461d3684cf

--- contracts/token/MultiCurrency.sol
+++ contracts/token/MultiCurrency.sol
@@ -3,120 +3,68 @@
 pragma solidity ^0.6.0;
 
 library MultiCurrency {
+    address constant private precompile = address(0x0000000000000000000000000000000000000400);
+
     function name() internal view returns (string memory) {
-        bytes memory input = abi.encodeWithSignature("name()");
-
-        // Dynamic arrays will add the array size to the front of the array, so need extra 32 bytes.
-        uint input_size = input.length + 32;
-
-        // Acala mirrored token symbol should not more than 32 bytes. More than 32 bytes will be truncated.
-        uint256[3] memory output;
-
+        (bool success, bytes memory returnData) = precompile.staticcall(abi.encodeWithSignature("name()"));
         assembly {
-            if iszero(
-                staticcall(gas(), 0x0000000000000000400, input, input_size, output, 0x60)
-            ) {
-                revert(0, 0)
+            if eq(success, 0) {
+                revert(add(returnData, 0x20), returndatasize())
             }
         }
 
-        bytes memory result = abi.encodePacked(output);
-        (string memory name_res) = abi.decode(result, (string));
-
-        return name_res;
+        return abi.decode(returnData, (string));
     }
 
     function symbol() internal view returns (string memory) {
-        bytes memory input = abi.encodeWithSignature("symbol()");
-
-        // Dynamic arrays will add the array size to the front of the array, so need extra 32 bytes.
-        uint input_size = input.length + 32;
-
-        // Acala mirrored token symbol should not more than 32 bytes. More than 32 bytes will be truncated.
-        uint256[3] memory output;
-
+        (bool success, bytes memory returnData) = precompile.staticcall(abi.encodeWithSignature("symbol()"));
         assembly {
-            if iszero(
-                staticcall(gas(), 0x0000000000000000400, input, input_size, output, 0x60)
-            ) {
-                revert(0, 0)
+            if eq(success, 0) {
+                revert(add(returnData, 0x20), returndatasize())
             }
         }
 
-        bytes memory result = abi.encodePacked(output);
-        (string memory symbol_res) = abi.decode(result, (string));
-
-        return symbol_res;
+        return abi.decode(returnData, (string));
     }
 
     function decimals() internal view returns (uint8) {
-        bytes memory input = abi.encodeWithSignature("decimals()");
-
-        // Dynamic arrays will add the array size to the front of the array, so need extra 32 bytes.
-        uint input_size = input.length + 32;
-
-        uint256[1] memory output;
-
+        (bool success, bytes memory returnData) = precompile.staticcall(abi.encodeWithSignature("decimals()"));
         assembly {
-            if iszero(
-                staticcall(gas(), 0x0000000000000000400, input, input_size, output, 0x20)
-            ) {
-                revert(0, 0)
+            if eq(success, 0) {
+                revert(add(returnData, 0x20), returndatasize())
             }
         }
 
-        return uint8(output[0]);
+        return abi.decode(returnData, (uint8));
     }
 
     function totalSupply() internal view returns (uint256) {
-        bytes memory input = abi.encodeWithSignature("totalSupply()");
-
-        // Dynamic arrays will add the array size to the front of the array, so need extra 32 bytes.
-        uint input_size = input.length + 32;
-
-        uint256[1] memory output;
-
+        (bool success, bytes memory returnData) = precompile.staticcall(abi.encodeWithSignature("totalSupply()"));
         assembly {
-            if iszero(
-                staticcall(gas(), 0x0000000000000000400, input, input_size, output, 0x20)
-            ) {
-                revert(0, 0)
+            if eq(success, 0) {
+                revert(add(returnData, 0x20), returndatasize())
             }
         }
 
-        return output[0];
+        return abi.decode(returnData, (uint256));
     }
 
     function balanceOf(address account) internal view returns (uint256) {
-        bytes memory input = abi.encodeWithSignature("balanceOf(address)", account);
-
-        // Dynamic arrays will add the array size to the front of the array, so need extra 32 bytes.
-        uint input_size = input.length + 32;
-
-        uint256[1] memory output;
-
+        (bool success, bytes memory returnData) = precompile.staticcall(abi.encodeWithSignature("balanceOf(address)", account));
         assembly {
-            if iszero(
-                staticcall(gas(), 0x0000000000000000400, input, input_size, output, 0x20)
-            ) {
-                revert(0, 0)
+            if eq(success, 0) {
+                revert(add(returnData, 0x20), returndatasize())
             }
         }
 
-        return output[0];
+        return abi.decode(returnData, (uint256));
     }
 
-    function transfer(address sender, address recipient, uint256 amount) internal view {
-        bytes memory input = abi.encodeWithSignature("transfer(address,address,uint256)", sender, recipient, amount);
-
-        // Dynamic arrays will add the array size to the front of the array, so need extra 32 bytes.
-        uint input_size = input.length + 32;
-
+    function transfer(address sender, address recipient, uint256 amount) internal {
+        (bool success, bytes memory returnData) = precompile.call(abi.encodeWithSignature("transfer(address,address,uint256)", sender, recipient, amount));
         assembly {
-            if iszero(
-                staticcall(gas(), 0x0000000000000000400, input, input_size, 0x00, 0x00)
-            ) {
-                revert(0, 0)
+            if eq(success, 0) {
+                revert(add(returnData, 0x20), returndatasize())
             }
         }
     }
