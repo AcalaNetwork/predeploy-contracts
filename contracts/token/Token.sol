0595c9ec826c0c3fbdd279b71d3ffb1da3473fb0
Ermal Kaleci <ermalkaleci@gmail.com>
2022-03-09 23:32:35 +0100

    - transferFrom should update allowance before transfer - make sure amount is not gt u128::max

--- contracts/token/Token.sol
+++ contracts/token/Token.sol
@@ -96,8 +96,8 @@ contract Token is IERC20 {
      * `amount`.
      */
     function transferFrom(address sender, address recipient, uint256 amount) public override returns (bool) {
-        _transfer(sender, recipient, amount);
         _approve(sender, msg.sender, _allowances[sender][msg.sender].sub(amount, "ERC20: transfer amount exceeds allowance"));
+        _transfer(sender, recipient, amount);
         return true;
     }
 
@@ -154,6 +154,7 @@ contract Token is IERC20 {
     function _transfer(address sender, address recipient, uint256 amount) internal {
         require(sender != address(0), "ERC20: transfer from the zero address");
         require(recipient != address(0), "ERC20: transfer to the zero address");
+        require(amount <= type (uint128).max, "ERC20: transfer amount exceeds uint128::max");
 
         MultiCurrency.transfer(sender, recipient, amount);
 
