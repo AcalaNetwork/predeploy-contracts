d8909cde9c4aa268ef47ee0e443e837b3bda3b6f
zjb0807 <zjb0807@qq.com>
2022-03-18 17:59:59 +0800

    cherry-pick 92b78ef9c9cee34f8ddb83a4a3888ba60689ac5b

--- generate/generate.js
+++ generate/generate.js
@@ -20,33 +20,32 @@ const generate = async () => {
   // compile to generate contracts json.
   await exec('yarn truffle-compile');
 
+  const { bytecode: token } = require(`../build/contracts/Token.json`);
   const tokenList = tokens.reduce((output, { symbol, address }) => {
-    return [...output, [symbol, ethers.utils.getAddress(address), ""]];
+    return [...output, [symbol, ethers.utils.getAddress(address), token]];
   }, []);
 
   let bytecodes = [];
-  const { bytecode: token } = require(`../build/contracts/Token.json`);
-  bytecodes.push(['Token', ethers.utils.getAddress('0x0000000000000000000000000000000000000800'), token]);
-
-  // add NFT bytecodes
-  const { bytecode: nft } = require(`../build/contracts/NFT.json`);
-  bytecodes.push(['NFT', ethers.utils.getAddress('0x0000000000000000000000000000000000000801'), nft]);
-
   // add EVM bytecodes
   const { bytecode: evm } = require(`../build/contracts/EVM.json`);
-  bytecodes.push(['EVM', ethers.utils.getAddress('0x0000000000000000000000000000000000000802'), evm]);
+  bytecodes.push(['EVM', ethers.utils.getAddress('0x0000000000000000000000000000000000000800'), evm]);
 
   // add Oracle bytecodes
   const { bytecode: oracle } = require(`../build/contracts/Oracle.json`);
-  bytecodes.push(['Oracle', ethers.utils.getAddress('0x0000000000000000000000000000000000000803'), oracle]);
+  bytecodes.push(['Oracle', ethers.utils.getAddress('0x0000000000000000000000000000000000000801'), oracle]);
 
   // add Schedule bytecodes
   const { bytecode: schedule } = require(`../build/contracts/Schedule.json`);
-  bytecodes.push(['Schedule', ethers.utils.getAddress('0x0000000000000000000000000000000000000804'), schedule]);
+  bytecodes.push(['Schedule', ethers.utils.getAddress('0x0000000000000000000000000000000000000802'), schedule]);
 
   // add DEX bytecodes
   const { bytecode: dex } = require(`../build/contracts/DEX.json`);
-  bytecodes.push(['DEX', ethers.utils.getAddress('0x0000000000000000000000000000000000000805'), dex]);
+  bytecodes.push(['DEX', ethers.utils.getAddress('0x0000000000000000000000000000000000000803'), dex]);
+
+  // Maybe each nft will deploy a contract, like the mirrored token.
+  // add NFT bytecodes
+  // const { bytecode: nft } = require(`../build/contracts/NFT.json`);
+  // bytecodes.push(['NFT', ethers.utils.getAddress('0x0000000000000000000000000000000000000804'), nft]);
 
   // merge tokenList into bytecodes
   bytecodes = tokenList.concat(bytecodes);
