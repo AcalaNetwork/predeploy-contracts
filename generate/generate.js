1121cb8ce202632f08404402e2bdd934e8e1c84e
zjb0807 <zjb0807@qq.com>
2022-05-17 00:04:03 +0800

    update bytecodes

--- generate/generate.js
+++ generate/generate.js
@@ -42,10 +42,18 @@ const generate = async () => {
   const { bytecode: dex } = await hre.artifacts.readArtifact("DEX");
   bytecodes.push(['DEX', ethers.utils.getAddress('0x0000000000000000000000000000000000000803'), dex]);
 
+  // add StableAsset bytecodes
+  // const { bytecode: stableAsset } = await hre.artifacts.readArtifact("StableAsset");
+  // bytecodes.push(['StableAsset', ethers.utils.getAddress('0x0000000000000000000000000000000000000804'), stableAsset]);
+
+  // add EVMAccounts bytecodes
+  const { bytecode: evmAccounts } = await hre.artifacts.readArtifact("EVMAccounts");
+  bytecodes.push(['EVMAccounts', ethers.utils.getAddress('0x0000000000000000000000000000000000000806'), evmAccounts]);
+
   // Maybe each nft will deploy a contract, like the mirrored token.
   // add NFT bytecodes
   // const { bytecode: nft } = require(`../build/contracts/NFT.json`);
-  // bytecodes.push(['NFT', ethers.utils.getAddress('0x0000000000000000000000000000000000000804'), nft]);
+  // bytecodes.push(['NFT', ethers.utils.getAddress('0x000000000000000000000000000000000000080X'), nft]);
 
   // merge tokenList into bytecodes
   bytecodes = tokenList.concat(bytecodes);
