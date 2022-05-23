b5c223d878dcfc3d48b675f4db446763559c9196
zjb0807 <zjb0807@qq.com>
2022-05-23 09:26:35 +0800

    Improve evm accounts (#101)
    * add claimDefaultEvmAddress
    
    * update comments
    
    * update README.md

--- contracts/evm-accounts/IEVMAccounts.sol
+++ contracts/evm-accounts/IEVMAccounts.sol
@@ -3,9 +3,14 @@
 pragma solidity ^0.8.0;
 
 interface IEVMAccounts {
+    event ClaimAccount(address indexed sender, bytes32 indexed accountId, address indexed evmAddress);
+
     // Returns the AccountId used to generate the given EvmAddress.
     function getAccountId(address evmAddress) external view returns (bytes32);
 
     // Returns the EvmAddress associated with a given AccountId or the underlying EvmAddress of the AccountId.
     function getEvmAddress(bytes32 accountId) external view returns (address);
+
+    // Claim account mapping between AccountId and a generated EvmAddress based off of the AccountId.
+    function claimDefaultEvmAddress(bytes32 accountId) external returns (bool);
 }
