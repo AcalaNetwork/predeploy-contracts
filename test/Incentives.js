d2c978d08a87729c2edde535ac4ee01d3f822e2b
ferrell-code <automatedcharlesferrell@gmail.com>
2022-07-28 19:28:45 -0400

    update incentives tests

--- test/Incentives.js
+++ test/Incentives.js
@@ -1,16 +1,23 @@
 const { expect } = require('chai');
 const { Contract, BigNumber } = require('ethers');
-const { INCENTIVES, LDOT, ACA } = require('../contracts/utils/MandalaAddress');
+const { INCENTIVES, LDOT, ACA, LP_DOT_AUSD, LP_ACA_AUSD } = require('../contracts/utils/MandalaAddress');
 const { getTestProvider, testPairs } = require('./util/utils');
 
 const IncentivesContract = require('../artifacts/contracts/incentives/Incentives.sol/Incentives.json');
 const ERC20Contract = require('../artifacts/contracts/token/Token.sol/Token.json');
-const { idText } = require('typescript');
 // Value of 0 represents loans and value of 1 represents dex
 const PoolId = {
     Loans: 0,
     Dex: 1,
 };
+
+const NULL_ADDRESS = '0x0000000000000000000000000000000000000000';
+
+const formatAmount = (amount) => {
+  return amount.replace(/_/g, '');
+};
+
+const FixedU128 = BigNumber.from(formatAmount('1_000_000_000_000_000_000'));
 
 describe('Incentives Contract', function () {
     let instance;
@@ -34,9 +41,40 @@ describe('Incentives Contract', function () {
 
         describe("getIncentiveRewardAmount", function () {
             it("works", async function () {
+                await provider.api.tx.sudo.sudo(
+                    provider.api.tx.incentives.updateIncentiveRewards([[{Loans: {Token: 'ACA'}}, [[{ Token: 'ACA' }, 100]]]])
+                )
+                .signAndSend(testPairs.alice.address);
                 const rewardAmount = await instance.getIncentiveRewardAmount(PoolId.Loans, ACA, ACA);
 
                 expect(rewardAmount).to.be.equal(100);
+            });
+
+            it("reverts when input bad PoolId Value", async function () {
+                expect(await instance.getIncentiveRewardAmount(2, ACA, ACA)).to.be.reverted;
+            });
+        });
+
+        describe("getDexRewardRate", function () {
+            it('empty storage returns 0', async function () {
+                const rewardRate = await instance.getDexRewardRate(LP_DOT_AUSD);
+
+                expect(rewardRate).to.be.equal(0);
+            });
+
+            it("works", async function () {
+                const Rate = FixedU128.div(BigNumber.from('10')); // 1/10
+                await provider.api.tx.sudo.sudo(
+                    provider.api.tx.incentives.updateDexSavingRewards([[{Dex: {DexShare: [{Token: 'ACA'}, {Token: 'AUSD'}]}}, Rate]])
+                )
+                .signAndSend(testPairs.alice.address);
+                const rewardRate = await instance.getDexRewardRate(LP_ACA_AUSD);
+
+                expect(rewardRate).to.be.equal(Rate);
+            });
+
+            it("null currency reverts", async function () {
+                expect(await instance.getDexRewardRate(NULL_ADDRESS)).to.be.reverted;
             });
         });
     });
