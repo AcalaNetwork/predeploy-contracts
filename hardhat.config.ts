2b81c1c43ff7ce25b2fc5493848f213364a1d0f7
zjb0807 <zjb0807@qq.com>
2023-06-13 19:39:58 +0800

    verify contracts (#150)
    * verify contracts
    
    * update config
    
    * fix build

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,12 @@
 import { HardhatUserConfig, task } from "hardhat/config";
 import '@nomiclabs/hardhat-ethers';
 import '@nomiclabs/hardhat-waffle';
+import "@nomiclabs/hardhat-etherscan";
 import '@primitivefi/hardhat-dodoc';
 import '@typechain/hardhat';
+
+import * as dotenv from "dotenv";
+dotenv.config();
 
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
@@ -19,13 +23,87 @@ const config: HardhatUserConfig = {
   },
   networks: {
     mandala: {
-      url: 'http://127.0.0.1:8545',
+      url: 'http://127.0.0.1:8545', // https://eth-rpc-tc9.aca-staging.network
       accounts: {
-        mnemonic: 'fox sight canyon orphan hotel grow hedgehog build bless august weather swarm',
+        mnemonic: process.env.MNEMONIC || 'fox sight canyon orphan hotel grow hedgehog build bless august weather swarm',
         path: "m/44'/60'/0'/0",
       },
       chainId: 595
     },
+    karuraTestnet: {
+      url: 'https://eth-rpc-karura-testnet.aca-staging.network',
+      accounts: {
+        mnemonic: process.env.MNEMONIC || '',
+      },
+      chainId: 596,
+      timeout: 120000,
+    },
+    acalaTestnet: {
+      url: 'https://eth-rpc-acala-testnet.aca-staging.network',
+      accounts: {
+        mnemonic: process.env.MNEMONIC || '',
+      },
+      chainId: 597,
+      timeout: 120000,
+    },
+    karura: {
+      url: 'https://eth-rpc-karura.aca-api.network',
+      accounts: {
+        mnemonic: process.env.MNEMONIC || '',
+      },
+      chainId: 686,
+      timeout: 120000,
+    },
+    acala: {
+      url: 'https://eth-rpc-acala.aca-api.network',
+      accounts: {
+        mnemonic: process.env.MNEMONIC || '',
+      },
+      chainId: 787,
+      timeout: 120000,
+    }
+  },
+  etherscan: {
+    apiKey: {
+      karuraTestnet: 'no-api-key-needed',
+      acalaTestnet: 'no-api-key-needed',
+      karura: 'no-api-key-needed',
+      acala: 'no-api-key-needed',
+    },
+    customChains: [
+      {
+        network: "karuraTestnet",
+        chainId: 596,
+        urls: {
+          apiURL: "https://blockscout.karura-testnet.aca-staging.network/api",
+          browserURL: "https://blockscout.karura-testnet.aca-staging.network",
+        },
+      },
+      {
+        network: "acalaTestnet",
+        chainId: 597,
+        urls: {
+          apiURL: "https://blockscout.acala-dev.aca-dev.network/api",
+          browserURL: "https://blockscout.acala-dev.aca-dev.network",
+        },
+      },
+      {
+        network: "karura",
+        chainId: 686,
+        urls: {
+          apiURL: "https://blockscout.karura.network/api",
+          browserURL: "https://blockscout.karura.network",
+        },
+      },
+      {
+        network: "acala",
+        chainId: 787,
+        urls: {
+          apiURL: "https://blockscout.acala.network/api",
+          browserURL: "https://blockscout.acala.network",
+        },
+      }
+    ],
   },
   typechain: {
     outDir: './contracts/typechain'
--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,37 +1,16 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// When running the script with `npx hardhat run <script>` you'll find the Hardhat
-// Runtime Environment's members available in the global scope.
 import { ethers } from "hardhat";
 
 async function main() {
-  const hre = await import("hardhat");
-  // Hardhat always runs the compile task when running scripts with its command
-  // line interface.
-  //
-  // If this script is run directly using `node` you may want to call compile
-  // manually to make sure everything is compiled
-  // await hre.run('compile');
+  const [deployer] = await ethers.getSigners();
+  console.log(`Deploying contracts with the account: ${deployer.address}`);
 
   // We get the contract to deploy
-  const evm = await ethers.getContractFactory("EVM");
-  //const e = await evm.deploy();
+  const xtokens = await ethers.getContractFactory("Xtokens", deployer);
+  const res = await xtokens.deploy();
 
-  //await e.deployed();
-
-  //console.log("EVM deployed to:", e.address);
-
-  const res = await hre.network.provider.send("hardhat_setCode", [
-    "0x0d2026b3EE6eC71FC6746ADb6311F6d3Ba1C000B",
-    evm.bytecode
-  ]);
-
-  console.log("EVM deployed to:", res);
+  console.log('XTOKEN address: ' + res.address);
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
 main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
